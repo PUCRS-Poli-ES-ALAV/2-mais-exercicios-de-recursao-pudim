a5f9cf28766a28d7d5c9be1301e24dc2432fb40f
MagicManatee <arthur.ibarra@edu.pucrs.br>
2021-08-09 19:44:03 -0300

    Fatorial feito.

--- App.java
+++ App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        
+        System.out.println("Fatorial of 7: " + Recursion.fatorial(7));
     }
 }
--- Recursion.java
+++ Recursion.java
@@ -1,7 +1,17 @@
+import java.security.InvalidAlgorithmParameterException;
+
 /**
  * Recursion
  */
 public class Recursion {
 
-    
+    public static double fatorial(int n) throws IllegalArgumentException {
+        if(n < 0){
+            throw new IllegalArgumentException("Negative numbers aren't allowed.");
+        }
+        if(n == 0 || n == 1){
+            return 1;
+        }
+        return n * fatorial(n - 1);
+    }
 }
